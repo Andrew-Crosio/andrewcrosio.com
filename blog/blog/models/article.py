58b145a62d3daa12a96a0378ddb3662034265593
Andrew Crosio <andrew.crosio@gmail.com>
2014-04-12 21:26:53 +0400

    adding autoslug field and migration

--- blog/blog/models/article.py
+++ blog/blog/models/article.py
@@ -2,10 +2,12 @@ from django.contrib.auth.models import User
 from django.db import models
 
 from blog.models.base import CreatedAtAndUpdatedAtModel
+from blog.models.fields import AutoSlugField
 
 
 class Article(CreatedAtAndUpdatedAtModel):
     title = models.CharField(max_length=250)
+    slug = AutoSlugField(fields=['title'], editable=False)
     author = models.ForeignKey(User, related_name='articles')
     content = models.TextField()
 
