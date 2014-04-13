14c03ac1934362f7db7f626a6d9315ae606f4e3e
Andrew Crosio <andrew.crosio@gmail.com>
2014-04-13 21:47:04 +0400

    adding gevent logic

--- blog/blog/wsgi.py
+++ blog/blog/wsgi.py
@@ -6,9 +6,28 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
-
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blog.conf.settings")
 
 from django.core.wsgi import get_wsgi_application
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blog.conf.settings")
+
 application = get_wsgi_application()
+
+
+def main():
+    from gevent import monkey
+    from gevent.wsgi import WSGIServer
+
+    monkey.patch_all()
+
+    server = WSGIServer(('0.0.0.0', 8080), application)
+
+    print 'Starting gevent server on http://0.0.0.0:8080'
+
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
